4bf8375a6dd855264a416c615438e235e0139806
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-09-19 03:00:48 +0200

    Initial release

--- GTAV-DLCPacks-Hasher/Form1.Designer.cs
+++ GTAV-DLCPacks-Hasher/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(643, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select GTA V Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 71);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(642, 476);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(667, 559);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(683, 598);
+            this.MinimumSize = new System.Drawing.Size(683, 598);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "GTA V File Hasher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- GTAV-DLCPacks-Hasher/Form1.cs
+++ GTAV-DLCPacks-Hasher/Form1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Windows.Forms;
 
 namespace GTAV_DLCPacks_Hasher
@@ -16,5 +18,110 @@ namespace GTAV_DLCPacks_Hasher
         {
             InitializeComponent();
         }
+        private string hashList = "";
+        private string[] filePath;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string hashList = "";
+            folderBrowserDialog1.ShowDialog();
+            filePath = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*", SearchOption.AllDirectories).OrderBy(p => p).ToArray();
+            if (!filePath[0].Contains("Grand Theft Auto V"))
+            {
+                MessageBox.Show("Error: You did not select a GTA (root) folder.");
+                return;
+            }
+            button1.Text = "Loading...";
+            UseWaitCursor = true;
+            //foreach (var file in filePath)
+            //{
+                //hashList += HashFile(file);
+
+            backgroundWorker1.RunWorkerAsync(filePath);
+            button1.Enabled = false;
+
+            //using (var sha1 = SHA1.Create())
+            //{
+            //byte[] contentBytes = File.ReadAllBytes(filePath[i]);
+            //sha1.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+            //sha1.TransformFinalBlock(new byte[0], 0, 0);
+            //hashList += "File: " + filePath[i] + "\nSHA1: " + BitConverter.ToString(sha1.Hash).Replace("-", "").ToLower() + "\n";
+            //byte[] contentBytes = File.ReadAllBytes(file);
+            //sha1.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+            //sha1.TransformFinalBlock(new byte[0], 0, 0);
+            //hashList += "File: " + file + "\nSHA1: " + BitConverter.ToString(sha1.Hash).Replace("-", "").ToLower() + "\n";
+            //}
+
+            //}
+
+        }
+        private void appendToHashString(string hash, string filepath)
+        {
+            hashList += "Filepath: " + filepath + "\nSHA1 hash: " + hash + "\n";
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < filePath.Length; i++) 
+            {
+                Console.WriteLine(filePath[i]);
+                //backgroundWorker1.ReportProgress(i);
+                //int progress = (i * 100) / (filePath.Count() * 100);
+                //Console.WriteLine(progress + "% complete.");
+                //richTextBox1.Text = richTextBox1.Text + "\nHasing: " + filePath[i];
+                //backgroundWorker1.ReportProgress(progress);
+                //backgroundWorker1.ReportProgress(((i + 1) / filePath.Length) * 100);
+                Stream file = null;
+                using (file = File.OpenRead(filePath[i].ToString()))
+                {
+                    byte[] fileBuffer = null;
+                    int bytesRead = 0;
+                    long size = file.Length;
+                    long totalBytesRead = 0;
+                    using (HashAlgorithm hasher = SHA1.Create())
+                    {
+                        do
+                        {
+                            fileBuffer = new byte[4096];
+                            bytesRead = file.Read(fileBuffer, 0, fileBuffer.Length);
+                            totalBytesRead += bytesRead;
+                            hasher.TransformBlock(fileBuffer, 0, bytesRead, null, 0);
+                        } while (bytesRead != 0);
+                        hasher.TransformFinalBlock(fileBuffer, 0, 0);
+                        StringBuilder hash2 = new StringBuilder(40);
+                        byte[] hashBytes = hasher.Hash;
+                        foreach (byte b in hashBytes)
+                            hash2.Append(b.ToString("X2").ToUpper());
+                        appendToHashString(hash2.ToString(), filePath[i]);
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //progressBar1.Value = e.ProgressPercentage;
+            //richTextBox1.Text = e.ProgressPercentage + "% complete.";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Console.WriteLine(hashList);
+            richTextBox2.Text = hashList;
+            UseWaitCursor = false;
+            button1.Text = "Done (Click me to select a new folder)";
+            button1.Enabled = true;
+        }
+        //private string HashFile(String filepath)
+        //{
+        //    //string sha1Hash = "";
+        //    //using (var sha1 = SHA1.Create())
+        //    //{
+        //    //    byte[] contentBytes = File.ReadAllBytes(filepath);
+        //    //    sha1.TransformFinalBlock(contentBytes, 0, 0);
+        //    //    sha1Hash = BitConverter.ToString(sha1.Hash).Replace("-", "").ToUpper();
+        //    //}
+
+        //    string hashEntry = "Filepath: " + filepath + "\nSHA1 hash: " + sha1Hash + "\n";
+        //    return hashEntry;
+        //}
     }
 }
